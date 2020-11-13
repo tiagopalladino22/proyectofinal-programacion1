85fd4afd2cafa61c7a2eb72ed210e07b09ecd646
Tiago Palladino <tiagopalladino22@gmail.com>
2020-11-13 15:11:30 -0300

    link a pagina de detalle

--- js/master.js
+++ js/master.js
@@ -10,7 +10,7 @@ window.addEventListener ('load', function(){
         //console.log(populares.results[0].poster_path);
         populares.results.forEach(pelicula => {
             let articulo = document.createElement ('article')
-            articulo.innerHTML += `<img src='https://image.tmdb.org/t/p/w500${pelicula.poster_path}' alt='${pelicula.title}'>
+            articulo.innerHTML += `<a href="detalle.html?idSerie=${pelicula.id}"><img src='https://image.tmdb.org/t/p/w500${pelicula.poster_path}' alt='${pelicula.title}'></a>
             `
             peliculas.append(articulo)
         });
@@ -26,7 +26,7 @@ fetch('https://api.themoviedb.org/3/tv/popular?api_key=942febef73deb21c3f70ec6b0
         //console.log(populares.results[0].poster_path);
         seriesPopulares.results.forEach(serie => {
             let articulo = document.createElement ('article')
-            articulo.innerHTML += `<img src='https://image.tmdb.org/t/p/w500${serie.poster_path}' alt='${serie.title}'>
+            articulo.innerHTML += `<a href="detalle.html?idSerie=${serie.id}"><img src='https://image.tmdb.org/t/p/w500${serie.poster_path}' alt='${serie.title}'></a>
             `
             series.append(articulo)
         });
@@ -41,7 +41,7 @@ fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=942febef73deb21c3f70
         //console.log(populares.results[0].poster_path);
         ultimosEstrenos.results.forEach(estreno => {
             let articulo = document.createElement ('article')
-            articulo.innerHTML += `<img src='https://image.tmdb.org/t/p/w500${estreno.poster_path}' alt='${estreno.title}'>
+            articulo.innerHTML += `<a href="detalle.html?idSerie=${estreno.id}"><img src='https://image.tmdb.org/t/p/w500${estreno.poster_path}' alt='${estreno.title}'></a>
             `
             estrenos.append(articulo)
         });

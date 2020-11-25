ce61de1e4f3b70a918f9dfbe323ca85c3ee4d03c
juanagomezprieto <jgomezprieto@udesa.edu.ar>
2020-11-25 19:43:59 -0300

    buscador

--- js/buscador.js
+++ js/buscador.js
@@ -1,40 +1,117 @@
-window.addEventListener('load', function(){
-    let peliculas = document.querySelector('.peliculas');
-    //console.log(peliculas);
-    fetch('https://api.themoviedb.org/3/movie/popular?api_key=17c89d9fa3c16b97f05929af9001eb2d&language=en-US&page=1')
-    .then(function(respuesta){
-        return respuesta.json()
+window.onload=function(){
+
+    var APIkey="23b5083840125a6643dffb1e3daa3bff"
+    var URLPoster="https://image.tmdb.org/t/p/original/"
+  
+  //genero elegido
+    fetch("https://api.themoviedb.org/3/genre/tv/list?api_key=" + APIkey)
+    .then(function (response) {
+      return response.json();
+      })
+    .then(function (seriesGeneros) {
+      console.log(seriesGeneros);
+      for (var i = 0; i < seriesGeneros.genres.length; i++) {
+        document.querySelector("select.generoelegido").innerHTML += "<option value="+seriesGeneros.genres[i].id+">"+seriesGeneros.genres[i].name+"</option>"
+        }})
+  
+  //genero descartado
+        fetch("https://api.themoviedb.org/3/genre/tv/list?api_key=" + APIkey)
+        .then(function (response) {
+          return response.json();
+          })
+        .then(function (seriesGeneros) {
+          console.log(seriesGeneros);
+          for (var i = 0; i < seriesGeneros.genres.length; i++) {
+            document.querySelector("select.generodescartado").innerHTML += "<option value="+seriesGeneros.genres[i].id+">"+seriesGeneros.genres[i].name+"</option>"
+            }})
+  
+  //año
+  
+    for (var i = 1950; i < 2020; i++) {
+      document.querySelector("select.generoaño").innerHTML += "<option value="+i+">"+i+"</option>"
+    }
+  
+  //funcionalidad buscador avanzado
+  
+  document.querySelector("button.buscador").onclick=function(e){
+    e.preventDefault()
+  
+  document.querySelector("#seriesBuscadas").innerHTML=""
+  
+    var posicion1=document.querySelector("select.generoelegido").selectedIndex
+    var options1=document.querySelector("select.generoelegido").options
+  
+    var posicion2=document.querySelector("select.generodescartado").selectedIndex
+    var options2=document.querySelector("select.generodescartado").options
+  
+    var posicion3=document.querySelector("select.generoaño").selectedIndex
+    var options3=document.querySelector("select.generoaño").options
+  
+    var posicion4=document.querySelector("select.generoorden").selectedIndex
+    var options4=document.querySelector("select.generoorden").options
+  
+  
+    var link="https://api.themoviedb.org/3/discover/tv?api_key=23b5083840125a6643dffb1e3daa3bff&language=en-US"
+  
+    if (posicion1!=-1 || posicion1!=0) {
+      var link = link + "&with_genres=" + options1[posicion1].value
+    }
+      if (posicion2!=-1 || posicion2!=0) {
+        var link = link + "&without_genres=" + options2[posicion2].value
+  
+    }
+  
+    if (posicion3!=-1 || posicion3!=0) {
+      var link = link + "&first_air_date_year=" + options3[posicion3].value
+  
+  }
+  if (posicion4!=-1 || posicion4!=0) {
+    var link = link + "&sort_by=" + options4[posicion4].value
+  
+  }
+  if ((posicion1!=0) && (posicion2!=0) ) {
+    alert("Debe seleccionar unicamente una de las dos casillas de genero")
+  } else if (posicion1<1 && posicion2<1 && posicion3<1 && posicion4<1) {
+    alert("Debe seleccionar algun campo")
+  } else {
+  
+  
+  fetch(link)
+  .then(function (response) {
+    return response.json();
     })
-    .then(function(populares){
-        //console.log(populares.results);
-
-        //populares.results.forEach(pelicula => {
-            //let articulo = document.createElement('article')
-            //articulo.innerHTML += `<img src='https://image.tmdb.org/t/p/w500${pelicula.poster_path}' alt = '${pelicula.title}' />`
-            //peliculas.append(articulo)
-        //})
-            /*for(let i = 0; i < populares.results.length; i++){
-                //console.log(populares.results[i].poster_path);
-                let articulo = document.createElement('article')
-                articulo.innerHTML += `<img src='https://image.tmdb.org/t/p/w500${populares.results[i].poster_path}' alt = '${populares.results[i].title}' />`
-                peliculas.append(articulo)
-            }*/
-            //Otra forma de rfecorrer un array
-            //for (let i of populares.results) {
-            //    console.log(i.poster_path);
-            //}
-            for(let i = 0; i < populares.results.length; i++){
-                //console.log(populares.results[i].poster_path);
-                let articulo = document.createElement('article')
-                articulo.innerHTML += `<img src='https://image.tmdb.org/t/p/w500${populares.results[i].poster_path}' alt = '${populares.results[i].title}' />`
-                peliculas.append(articulo)
-            }
-
-
-
+  .then(function (seriesBuscadas) {
+    console.log(seriesBuscadas);
+    for (var i = 0; i < seriesBuscadas.results.length; i++) {
+      if (seriesBuscadas.results[i].poster_path!=null) {
+        document.querySelector("#seriesBuscadas").innerHTML += "<li><a href='detalle.html?idSerie="+seriesBuscadas.results[i].id+"'><img src="+URLPoster+seriesBuscadas.results[i].poster_path+" alt=''></a></li>"
+      }}
+  
+  }
+  )
+  }
+  }
+  
+  
+  //DROPDOWN NAV PARA LOS GENEROS DE LAS SERIES
+  
+  
+  fetch("https://api.themoviedb.org/3/genre/tv/list?api_key=" + APIkey)
+  .then(function (response) {
+    return response.json();
     })
-    .catch(function(error){
-        console.log(error)
-    })
-
-})
+  .then(function (seriesGeneros) {
+    console.log(seriesGeneros);
+    for (var i = 0; i < seriesGeneros.genres.length; i++) {
+      document.querySelector("#dropdowngeneros").innerHTML += "<li><a href='seriesporgenero.html?idGenero="+seriesGeneros.genres[i].id+"'>"+seriesGeneros.genres[i].name+"</a></li>"
+      }})
+  
+  
+  
+  
+  
+  
+  
+  
+  }
+  
--- js/resultados.js
+++ js/resultados.js
@@ -1,26 +1,49 @@
-window.addEventListener ('load', function(){
-    
-  fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=942febef73deb21c3f70ec6b055cb722")
-.then(function (response) {
-return response.json();
-})
-.then(function (listaGeneros) {
-console.log(listaGeneros);
-for (var i = 0; i < listaGeneros.genres.length; i++) {
-  document.querySelector("section.generos").innerHTML += "<li><a href='detalleGenero.html?idGenero="+listaGeneros.genres[i].id+"'>"+listaGeneros.genres[i].name+"</a></li>"
-  }})
+window.onload=function(){
+  var APIkey="23b5083840125a6643dffb1e3daa3bff"
+  var URLPoster="https://image.tmdb.org/t/p/original/"
+  var urlParams = new URLSearchParams(location.search);
+  var query = urlParams.get('buscador');
 
-  fetch("https://api.themoviedb.org/3/genre/tv/list?api_key=942febef73deb21c3f70ec6b055cb722")
-  .then(function (response) {
-    return response.json();
-    })
-  .then(function (listaGenerosSerie) {
-    console.log(listaGenerosSerie);
-    for (var i = 0; i < listaGenerosSerie.genres.length; i++) {
-      document.querySelector("section.generosSerie").innerHTML += "<li><a href='detalleGenero.html?idGenero="+listaGenerosSerie.genres[i].id+"'>"+listaGenerosSerie.genres[i].name+"</a></li>"
-      }})
+ //RESULTADOS DEL BUSCADOR
+
+   fetch("https://api.themoviedb.org/3/search/tv?api_key="+APIkey+"&language=en-US&query="+query+"&page=1")
+   .then(function (response) {
+     return response.json();
+   })
+   .then(function (resultadosBusqueda) {
+     console.log(resultadosBusqueda);
+    if (resultadosBusqueda.results.length==0) {
+      alert("Su busqueda no obtuvo resultados, por favor intente nuevamente.")
+    } else {
+
+
+     for (var i = 0; i < resultadosBusqueda.results.length; i++) {
+       if (resultadosBusqueda.results[i].poster_path != null) {
+         document.querySelector("#resultadosBusqueda").innerHTML += "<li><a href='detalle.html?idSerie="+resultadosBusqueda.results[i].id+"'><img src="+URLPoster+resultadosBusqueda.results[i].poster_path+" alt=''></a></li>"
+       }
+
+   }}
+
+   })
+
+   //DROPDOWN NAV PARA LOS GENEROS DE LAS SERIES
+
+
+   fetch("https://api.themoviedb.org/3/genre/tv/list?api_key=" + APIkey)
+   .then(function (response) {
+     return response.json();
+     })
+   .then(function (seriesGeneros) {
+     console.log(seriesGeneros);
+     for (var i = 0; i < seriesGeneros.genres.length; i++) {
+       document.querySelector("#dropdowngeneros").innerHTML += "<li><a href='seriesporgenero.html?idGenero="+seriesGeneros.genres[i].id+"'>"+seriesGeneros.genres[i].name+"</a></li>"
+       }})
+
+
+       //QUE SE BUSCO
+
+  document.querySelector('#lo_buscado').innerHTML = "Has buscado: " + query
+
 
 
 }
-
-)
